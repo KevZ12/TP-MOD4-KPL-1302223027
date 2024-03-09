2406424b14df1dffe1970be051b5858163cf358b
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-03-09 15:44:27 +0700

    “menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302223027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kodePos = new KodePos();
+
+            //menampilkan kode pos dari Kujasari
+            Console.WriteLine("kode pos dari "+ KodePos.Kelurahan.Kujangsari+ " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Kujangsari));
+            //menampilkan kode pos dari Wates
+            Console.WriteLine("kode pos dari " + KodePos.Kelurahan.Wates + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Wates));
+            //menampilkan kode pos dari Maleer
+            Console.WriteLine("kode pos dari " + KodePos.Kelurahan.Maleer + " adalah " + KodePos.getKodePos(KodePos.Kelurahan.Maleer));
+
+        }
+    }
+}
